7f383b4209b417006f4e796cc6b432f7a356bea4
ZDBryukhanov <zdbryukhanov@mai.education>
2023-12-02 02:35:42 +0300

    test for the visitor

--- laba06/src/main.cpp
+++ laba06/src/main.cpp
@@ -3,53 +3,47 @@
 #include "../include/characters/Elf.h"
 #include "../include/characters/Rogue.h"
 #include "../include/characters/NPCFactory.h"
+#include "../include/visitor/BattleVisitor.h"
 
 int main() {
 
     ConsoleObserver consoleObserver;
     FileObserver fileObserver;
 
-    NPC* bear = NPCFactory::createNPC(NPCType::Bear, "Bear1", Coordinates(100, 100));
-    NPC* bear2 = NPCFactory::createNPC(NPCType::Bear, "Bear2", Coordinates(101, 101));
-    NPC* elf = NPCFactory::createNPC(NPCType::Elf, "Elf1", Coordinates(200, 200));
-    NPC* elf2 = NPCFactory::createNPC(NPCType::Elf, "Elf2", Coordinates(201, 201));
-    NPC* rogue = NPCFactory::createNPC(NPCType::Rogue, "Rogue1", Coordinates(300, 300));
-    NPC* rogue2 = NPCFactory::createNPC(NPCType::Rogue, "Rogue2", Coordinates(301, 301));
-    NPC* rogue3 = NPCFactory::createNPC(NPCType::Rogue, "Rogue3", Coordinates(302, 302));
+    std::vector<NPC*> characters;
 
-    bear->addObserver(&consoleObserver);
-    bear2->addObserver(&consoleObserver);
-    elf->addObserver(&consoleObserver);
-    elf2->addObserver(&consoleObserver);
-    rogue->addObserver(&consoleObserver);
-    rogue2->addObserver(&consoleObserver);
-    rogue3->addObserver(&consoleObserver);
-    bear->addObserver(&fileObserver);
-    bear2->addObserver(&fileObserver);
-    elf->addObserver(&fileObserver);
-    elf2->addObserver(&fileObserver);
-    rogue->addObserver(&fileObserver);
-    rogue2->addObserver(&fileObserver);
-    rogue3->addObserver(&fileObserver);
+    characters.push_back(NPCFactory::createNPC(NPCType::Elf, "Elf1", Coordinates(200, 200)));
+    characters.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue1", Coordinates(300, 300)));
+    characters.push_back(NPCFactory::createNPC(NPCType::Bear, "Bear1", Coordinates(100, 100)));
+    characters.push_back(NPCFactory::createNPC(NPCType::Bear, "Bear2", Coordinates(101, 101)));
+    characters.push_back(NPCFactory::createNPC(NPCType::Elf, "Elf2", Coordinates(201, 201)));
+    characters.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue2", Coordinates(301, 301)));
+    characters.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue3", Coordinates(400, 400)));
 
-    bear->attack(elf);
-    bear->attack(bear2);
-    bear->attack(rogue);
 
-    std::cout << std::endl;
+    for (NPC* character: characters) {
+        character->addObserver(&consoleObserver);
+        character->addObserver(&fileObserver);
+    }
 
-    elf2->attack(bear);
-    elf2->attack(rogue);
+    BattleVisitor battleVisitor(200, characters);
 
-    std::cout << std::endl;
+    auto person = characters.begin();
+    while (person != characters.end()) {
+        NPC* target = *person;
+        target->accept(battleVisitor);
+        ++person;
+    }
 
-    rogue2->attack(bear);
-    rogue2->attack(elf2);
-    rogue2->attack(rogue3);
+    for (NPC* character: characters) {
+        std::cout << "Person name: " << character->getName() << std::endl;
+    }
 
-//    delete bear;
-//    delete elf;
-//    delete rogue;
+    for (NPC* character: characters) {
+        if (character) {
+            delete character;
+        }
+    }
 
     return 0;
 }
