04a74a109f9260975419066de483889f05ceb72c
ZDBryukhanov <zdbryukhanov@mai.education>
2023-12-02 02:34:26 +0300

    separation .cpp and .h files

--- laba06/include/characters/Bear.h
+++ laba06/include/characters/Bear.h
@@ -7,17 +7,9 @@ class Bear : public NPC {
 public:
     Bear(const std::string& name, const Coordinates& coords) : NPC(name, coords) {}
 
-    void attack(NPC* target) override {
-        if (dynamic_cast<Elf*>(target) != nullptr) {
-            notifyObservers(name + " is attacking and kill " + target->getName() + "!");
-            delete target;
-        } else {
-            notifyObservers(name + " is attacking " + target->getName());
-            target->defend();
-        }
-    }
+    void attack(NPC* target) override;
 
-    void defend() override {
-        notifyObservers("and " + name + " is defending.");
-    }
+    void defend() override;
+
+    void accept(Visitor& visitor) override;
 };
--- laba06/include/characters/Rogue.h
+++ laba06/include/characters/Rogue.h
@@ -6,18 +6,9 @@ class Rogue : public NPC {
 public:
     Rogue(const std::string& name, const Coordinates& coords) : NPC(name, coords) {}
 
-    void attack(NPC* target) override {
-        if (dynamic_cast<Rogue*>(target) != nullptr) {
-            notifyObservers(name + " is attacking and kill " + target->getName() + "!");
-            delete target;
-        } else {
-            notifyObservers(name + " is attacking " + target->getName());
-            target->defend();
-        }
-    }
+    void attack(NPC* target) override;
 
-    void defend() override {
-        notifyObservers("and " + name + " is defending.");
-    }
+    void defend() override;
 
+    void accept(Visitor& visitor) override;
 };
