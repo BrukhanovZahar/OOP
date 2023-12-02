04b37ef32b3c9eff95f7c440adb057defaeb1206
ZDBryukhanov <zdbryukhanov@mai.education>
2023-12-02 03:58:53 +0300

    change for serialize/deserialize

--- laba06/src/main.cpp
+++ laba06/src/main.cpp
@@ -1,6 +1,7 @@
-#include "../include/characters/NPC.h"
 #include "../include/characters/NPCFactory.h"
 #include "../include/battle/BattleManager.h"
+#include "../include/data/DataHandler.h"
+
 int main() {
 
     ConsoleObserver consoleObserver;
@@ -8,14 +9,17 @@ int main() {
 
     std::vector<NPC*> characters;
 
-    characters.push_back(NPCFactory::createNPC(NPCType::Elf, "Elf1", Coordinates(200, 200)));
-    characters.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue1", Coordinates(300, 300)));
-    characters.push_back(NPCFactory::createNPC(NPCType::Bear, "Bear1", Coordinates(100, 100)));
-    characters.push_back(NPCFactory::createNPC(NPCType::Bear, "Bear2", Coordinates(101, 101)));
-    characters.push_back(NPCFactory::createNPC(NPCType::Elf, "Elf2", Coordinates(201, 201)));
-    characters.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue2", Coordinates(301, 301)));
-    characters.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue3", Coordinates(400, 400)));
+//    characters.push_back(NPCFactory::createNPC(NPCType::Elf, "Elf1", Coordinates(200, 200)));
+//    characters.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue1", Coordinates(300, 300)));
+//    characters.push_back(NPCFactory::createNPC(NPCType::Bear, "Bear1", Coordinates(100, 100)));
+//    characters.push_back(NPCFactory::createNPC(NPCType::Bear, "Bear2", Coordinates(101, 101)));
+//    characters.push_back(NPCFactory::createNPC(NPCType::Elf, "Elf2", Coordinates(201, 201)));
+//    characters.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue2", Coordinates(301, 301)));
+//    characters.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue3", Coordinates(400, 400)));
+//
+//    DataHandler::serializeToFile(characters, "characters.json");
 
+    characters = DataHandler::deserializeFromFile("characters.json");
 
     for (NPC* character: characters) {
         character->addObserver(&consoleObserver);
