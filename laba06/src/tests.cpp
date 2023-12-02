c9a9d522af5d5c6d0c2f5e2971875ddf731ed467
ZDBryukhanov <zdbryukhanov@mai.education>
2023-12-02 14:10:11 +0300

    add more tests

--- laba06/src/tests.cpp
+++ laba06/src/tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "../include/characters/NPCFactory.h"
+#include "../include/data/DataHandler.h"
 
 class NPCFactoryTest : public testing::TestWithParam<std::tuple<NPCType, std::string, Coordinates>> {
 };
@@ -12,7 +13,6 @@ TEST_P(NPCFactoryTest, CreateNPC) {
 
     NPC* npc = NPCFactory::createNPC(type, name, coords);
 
-    // Проверяем, что объект создан и имеет правильные параметры
     ASSERT_NE(npc, nullptr);
     EXPECT_EQ(npc->getName(), name);
     EXPECT_EQ(npc->getCoordinates().x, coords.x);
@@ -21,7 +21,6 @@ TEST_P(NPCFactoryTest, CreateNPC) {
     delete npc;
 }
 
-// Задаем параметры для теста: Bear, Elf, Rogue с разными именами и координатами
 INSTANTIATE_TEST_SUITE_P(
         NPCFactoryTests,
         NPCFactoryTest,
@@ -32,6 +31,102 @@ INSTANTIATE_TEST_SUITE_P(
         )
 );
 
+TEST(NPCDeserializeTest, DeserializeNPC) {
+    std::vector<NPC*> rightNPC;
+    std::vector<NPC*> characters;
+
+    characters = DataHandler::deserializeFromFile("charactersTest.json");
+
+    rightNPC.push_back(NPCFactory::createNPC(NPCType::Elf, "Elf1", Coordinates(200, 200)));
+    rightNPC.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue1", Coordinates(300, 300)));
+    rightNPC.push_back(NPCFactory::createNPC(NPCType::Bear, "Bear1", Coordinates(100, 100)));
+    rightNPC.push_back(NPCFactory::createNPC(NPCType::Bear, "Bear2", Coordinates(101, 101)));
+    rightNPC.push_back(NPCFactory::createNPC(NPCType::Elf, "Elf2", Coordinates(201, 201)));
+    rightNPC.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue2", Coordinates(301, 301)));
+    rightNPC.push_back(NPCFactory::createNPC(NPCType::Rogue, "Rogue3", Coordinates(400, 400)));
+
+    for (int i{0}; i < rightNPC.size(); ++i) {
+        std::string typeCheck, typeRight;
+        std::string nameCheck, nameRight;
+
+        typeRight = rightNPC[i]->getType();
+        typeCheck = characters[i]->getType();
+        nameRight = rightNPC[i]->getName();
+        nameCheck = characters[i]->getName();
+        Coordinates coordinateCheck = rightNPC[i]->getCoordinates();
+        Coordinates coordinateRight = characters[i]->getCoordinates();
+
+        ASSERT_NE(rightNPC[i], nullptr);
+        ASSERT_NE(characters[i], nullptr);
+        EXPECT_EQ(typeRight, typeCheck);
+        EXPECT_EQ(nameCheck, nameRight);
+        EXPECT_EQ(coordinateCheck.x, coordinateRight.x);
+        EXPECT_EQ(coordinateCheck.y, coordinateRight.y);
+    }
+}
+
+TEST(NPCFightTest, ElfRogueNPC) {
+    std::string nameElf, nameRogue;
+    std::string nameForElf = "Elf1", nameForRogue = "Rogue1";
+    NPC* elf = NPCFactory::createNPC(NPCType::Elf, nameForElf, Coordinates(10, 10));
+    NPC* rogue = NPCFactory::createNPC(NPCType::Rogue, nameForRogue, Coordinates(12, 12));
+
+    rogue->attack(elf);
+    elf->attack(rogue);
+    nameElf = elf->getName();
+    nameRogue = rogue->getName();
+    Coordinates coordinateElf = elf->getCoordinates();
+    Coordinates coordinateRogue = rogue->getCoordinates();
+
+    EXPECT_EQ(nameElf, nameForElf);
+    EXPECT_EQ(nameRogue, "");
+    EXPECT_EQ(coordinateElf.x, 10);
+    EXPECT_EQ(coordinateElf.y, 10);
+    EXPECT_EQ(coordinateRogue.x, 0);
+    EXPECT_EQ(coordinateRogue.y, 0);
+}
+
+TEST(NPCFightTest, ElfBearNPC) {
+    std::string nameElf, nameBear;
+    std::string nameForElf = "Elf1", nameForBear = "Bear1";
+    NPC* elf = NPCFactory::createNPC(NPCType::Elf, nameForElf, Coordinates(10, 10));
+    NPC* bear = NPCFactory::createNPC(NPCType::Bear, nameForBear, Coordinates(12, 12));
+
+    elf->attack(bear);
+    bear->attack(elf);
+    nameElf = elf->getName();
+    nameBear = bear->getName();
+    Coordinates coordinateElf = elf->getCoordinates();
+    Coordinates coordinateRogue = bear->getCoordinates();
+
+    EXPECT_EQ(nameElf, "");
+    EXPECT_EQ(nameBear, nameForBear);
+    EXPECT_EQ(coordinateElf.x, 0);
+    EXPECT_EQ(coordinateElf.y, 0);
+    EXPECT_EQ(coordinateRogue.x, 12);
+    EXPECT_EQ(coordinateRogue.y, 12);
+}
+
+TEST(NPCFightTest, RogueBearNPC) {
+    std::string nameRogue, nameBear;
+    std::string nameForRogue = "Elf1", nameForBear = "Bear1";
+    NPC* rogue = NPCFactory::createNPC(NPCType::Rogue, nameForRogue, Coordinates(10, 10));
+    NPC* bear = NPCFactory::createNPC(NPCType::Bear, nameForBear, Coordinates(12, 12));
+
+    rogue->attack(bear);
+    bear->attack(rogue);
+    nameRogue = rogue->getName();
+    nameBear = bear->getName();
+    Coordinates coordinateElf = rogue->getCoordinates();
+    Coordinates coordinateRogue = bear->getCoordinates();
+
+    EXPECT_EQ(nameRogue, nameForRogue);
+    EXPECT_EQ(nameBear, nameForBear);
+    EXPECT_EQ(coordinateElf.x, 10);
+    EXPECT_EQ(coordinateElf.y, 10);
+    EXPECT_EQ(coordinateRogue.x, 12);
+    EXPECT_EQ(coordinateRogue.y, 12);
+}
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
